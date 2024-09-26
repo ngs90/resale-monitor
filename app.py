32b6cd7c09e15183da5460da7c8885be838e2f5e
ngs90 <nicolaj.schmit@hotmail.com>
2024-09-26 20:13:06 +0200

    update

--- app.py
+++ app.py
@@ -91,7 +91,16 @@ def home():
     print('HELLO!')
     print('request args: ', request.args)
     print('request data: ', request.data)
+
+    r = requests.post(f"https://graph.facebook.com/me/subscribed_apps?access_token={os.getenv('ACCESS_TOKEN')}", json={"subscribed_fields": ["messages", "messaging_postbacks", "messaging_referrals"]})
+    response = r.json()
+    if "success" in response:
+        if response["success"] == True:
+        print("Webhook subscriptions renewed")
+
     return f"Hello, World! Your Flask App is running on Heroku with HTTPS!"
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
